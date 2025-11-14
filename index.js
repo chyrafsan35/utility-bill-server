180080986e7d63a79221ea185927e84ccc77390a
unknown <mrcplabon@gmail.com>
2025-11-14 22:47:21 +0600

    env variable added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 3000;
@@ -7,6 +8,7 @@ const port = process.env.PORT || 3000;
 //middleware
 app.use(cors());
 app.use(express.json());
+
 
 app.get('/', (req, res) => {
     res.send('Server is running')
@@ -16,8 +18,7 @@ app.listen(port, () => {
     console.log(`Server is running on port : ${port}`)
 })
 
-
-const uri = "mongodb+srv://utilitydb:GFtQqCzCiy90Tdl9@cluster0.o0d4b4z.mongodb.net/?appName=Cluster0";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.o0d4b4z.mongodb.net/?appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
